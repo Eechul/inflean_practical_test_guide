07dcccdb3ebab2dc15a5e7599b1ab0f4f260eb61
이동철 <choise154@gmail.com>
2023-08-11 15:35:29 +0900

    프로젝트 생성

---
+++ src/main/java/sample/cafekiosk/spring/InfleanPracticalTestGuideApplication.java
@@ -0,0 +1,13 @@
+package sample.cafekiosk.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfleanPracticalTestGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfleanPracticalTestGuideApplication.class, args);
+    }
+
+}
