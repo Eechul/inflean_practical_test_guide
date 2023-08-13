832b03d330031ad44a9874972862a2bc8bf36eac
이동철 <choise154@gmail.com>
2023-08-13 21:51:26 +0900

    Spring & JPA 기반 테스트: Persistance(Repository) Layer 테스트

---
+++ src/main/java/sample/cafekiosk/spring/domain/product/Product.java
@@ -0,0 +1,41 @@
+package sample.cafekiosk.spring.domain.product;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sample.cafekiosk.spring.domain.BaseEntity;
+
+import static jakarta.persistence.GenerationType.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String productNumber; // 001, 002 등
+
+    @Enumerated(EnumType.STRING)
+    private ProductType type;
+
+    @Enumerated(EnumType.STRING)
+    private ProductSellingStatus sellingStatus;
+
+    private String name;
+
+    private int price;
+
+    @Builder
+    public Product(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
+}
