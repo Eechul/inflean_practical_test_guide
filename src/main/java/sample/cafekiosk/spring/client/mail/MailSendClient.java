afc1200336cef29d43aae896df34dff64cc919cd
이동철 <choise154@gmail.com>
2023-08-24 20:41:52 +0900

    @Mock과 @Spy의 차이점

--- src/main/java/sample/cafekiosk/spring/client/mail/MailSendClient.java
+++ src/main/java/sample/cafekiosk/spring/client/mail/MailSendClient.java
@@ -7,10 +7,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class MailSendClient {
 
-
     public boolean sendMail(String fromEmail, String toEmail, String subject, String content) {
         // 메일 전송
         log.info("메일 전송");
         throw new IllegalArgumentException("메일 전송");
+    }
+
+    public void a() {
+        log.info("a 메서드");
+    }
+
+    public void b() {
+        log.info("b 메서드");
+    }
+
+    public void c() {
+        log.info("c 메서드");
     }
 }
--- src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -4,9 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafekiosk.spring.client.mail.MailSendClient;
 import sample.cafekiosk.spring.client.mail.MailSendHistoryRepository;
@@ -23,6 +21,13 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class MailServiceTest {
 
+//    @Mock mock과 spy의 차이가 뭘까
+//    Mock은 객체의 모든 메서드를 stubbing 해주는 것이고
+//    Spy는 객체의 일부 메서드만 stubbing 해주는 것이다.
+//    그래서 Spy는 stubbing한 메서드를 제외한 나머지 메서드들의 동작을 확인할 수 있다.
+//    @Spy
+//    private MailSendClient mailSendClient;
+
     @Mock
     private MailSendClient mailSendClient;
 
@@ -36,8 +41,15 @@ class MailServiceTest {
     @Test
     void sendMail() {
         // given
-        when(mailSendClient.sendMail(anyString(), anyString(), anyString(), anyString()))
-                .thenReturn(true);
+        // @Mock을 쓸때, BDDMockito.given을 사용하면 given절에 맞게 사용할 수 있어서 좋다.
+//        Mockito.when(mailSendClient.sendMail(anyString(), anyString(), anyString(), anyString()))
+//                .thenReturn(true);
+        BDDMockito.given(mailSendClient.sendMail(anyString(), anyString(), anyString(), anyString()))
+                .willReturn(true);
+
+        // @Spy를 쓸때
+//        doReturn(true)
+//                .when(mailSendClient).sendMail(anyString(), anyString(), anyString(), anyString());
 
         // when
         boolean result = mailService.sendMail("from", "to", "subject", "content");
